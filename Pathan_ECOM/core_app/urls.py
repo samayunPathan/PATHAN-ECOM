5cfef4455b1a8b32db4596310828b4804ac88061
samayun <samayunp@gmail.com>
2024-03-22 18:30:43 +0600

    add image

--- Pathan_ECOM/core_app/urls.py
+++ Pathan_ECOM/core_app/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
 from core_app import views
+from django.conf import settings
+from django.contrib.staticfiles.urls import static ,staticfiles_urlpatterns
 
 app_name='core_app'
 
@@ -7,3 +9,8 @@ urlpatterns = [
     path('',views.font_page,name='font_page'),
     path('about/',views.about,name='about'),
 ]
+
+
+urlpatterns+=staticfiles_urlpatterns()
+urlpatterns+=static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
+
